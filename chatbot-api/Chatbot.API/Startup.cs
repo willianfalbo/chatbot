54805f46c2f6f1022e3e71fb62a8de51ca88a071
willianfalbo <willianfalbo@gmail.com>
2019-06-28 02:04:57 -0300

    enabled CORS

--- chatbot-api/Chatbot.API/Startup.cs
+++ chatbot-api/Chatbot.API/Startup.cs
@@ -17,14 +17,78 @@ namespace Microsoft.BotBuilderSamples
 {
     public class Startup
     {
+        private const string ALLOW_ANY_ORIGIN_POLICY = "ALLOW_ANY_ORIGIN_POLICY";
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // it should be at the top of the method
+            CORSPolicySetup(services);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            //AUTO MAPPER
             AutoMapperSetup(services);
 
+            ChatbotDISetup(services);
+
+            ApplicationDISetup(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            // it should be at the top of the method
+            app.UseCors(ALLOW_ANY_ORIGIN_POLICY);
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+        private static void CORSPolicySetup(IServiceCollection services)
+        {
+            // https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-2.2
+            // adds cross-origin resource sharing these service with other apps
+            services.AddCors(options =>
+            {
+                options.AddPolicy(ALLOW_ANY_ORIGIN_POLICY,
+                builder =>
+                {
+                    builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
+        }
+
+        private static void AutoMapperSetup(IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            var cfg = new MapperConfigurationExpression();
+            cfg.AddProfile<CompanyRegistryMappingProfile>();
+            cfg.AddProfile<DirectLineTokenMappingProfile>();
+
+            Mapper.Initialize(cfg);
+
+            Mapper.AssertConfigurationIsValid();
+        }
+
+        private static void ChatbotDISetup(IServiceCollection services)
+        {
             // Create the credential provider to be used with the Bot Framework Adapter.
             services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
 
@@ -43,43 +107,14 @@ namespace Microsoft.BotBuilderSamples
             services.AddSingleton<MainDialog>();
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, DialogAndWelcomeBot<MainDialog>>();
+        }
 
+        private static void ApplicationDISetup(IServiceCollection services)
+        {
             // custom classes
             services.AddSingleton<IAppSettings, AppSettings>();
             services.AddSingleton<ICompanyRegistryManager, CompanyRegistryManager>();
             services.AddSingleton<IDirectLineManager, DirectLineManager>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
-            //app.UseHttpsRedirection();
-            app.UseMvc();
-        }
-
-        private static void AutoMapperSetup(IServiceCollection services)
-        {
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            var cfg = new MapperConfigurationExpression();
-            cfg.AddProfile<CompanyRegistryMappingProfile>();
-            cfg.AddProfile<DirectLineTokenMappingProfile>();
-
-            Mapper.Initialize(cfg);
-
-            Mapper.AssertConfigurationIsValid();
         }
     }
 }
